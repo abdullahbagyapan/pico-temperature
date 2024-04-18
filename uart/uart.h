eb5a8b384a17aa84928515d812684ba918926258
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-18 23:19:00 +0300

    feat::uart module
    -add header file

---
+++ uart/uart.h
@@ -0,0 +1,42 @@
+/*
+ @module: UART
+ @author: Abdullah Bagyapan
+ @date:   18/04/2024
+*/
+
+/*================================== Libraries ==================================*/
+
+
+#include "stdint.h"
+
+
+/*================================== Functions ==================================*/
+
+
+/*
+ @brief:  Initialize UART
+
+ @author  Abdullah Bagyapan
+
+ @date    18/04/2024
+
+ @param   None
+
+ @return  None
+*/
+void UART_Init(void);
+
+
+
+/*
+ @brief:  Transmit a byte data by UART
+
+ @author  Abdullah Bagyapan
+
+ @date    18/04/2024
+
+ @param   The 1 byte data
+
+ @return  None
+*/
+void UART_PutChar(uint8_t ui8Data);
