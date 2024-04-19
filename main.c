f6a1da079479223a98f8651689c90cc9924ea42f
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-19 19:31:30 +0300

    feat:add main.c

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "pico/stdlib.h"
+
+#include "temperature/temperature.c"
+#include "uart/uart.h"
+
+
+int main() {
+
+    UART_Init();
+    TEMPERATURE_Init();
+
+    while (1) {
+
+        uint8_t ui8TemperatureCelsius = TEMPERATURE_Read();
+
+        UART_PutString(ui8TemperatureCelsius);
+        UART_PutChar('\n');
+
+        sleep_ms(1000);
+    }
+
+}
